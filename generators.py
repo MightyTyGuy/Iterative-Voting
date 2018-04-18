36f243e69e2f5c062778e2348c1514b0bd54df6d
Unknown <tyler.bowles@aggiemail.usu.edu>
2018-04-17 22:44:41 -0600

    Implemented Profile Generators

---
+++ generators.py
@@ -0,0 +1,22 @@
+import random
+
+# Create a profile of n ballots with 
+# Impartial Culture Assumption
+def impartial_culture(candidates, n):
+    return [random.sample(candidates, len(candidates)) for _ in range(n)]
+
+# Create a profile of n ballots with
+# the Polya-Eggenberger Model
+# Algorithm adapted from "Learning Agents For Iterative Voting"
+def polya_eggenberger(candidates, n, alpha):
+    N = []
+    beta = 1 / alpha - 1
+    for i in range(n):
+        p = random.random()
+        if p < beta / (beta + len(N)):
+            v = random.sample(candidates, len(candidates))
+        else:
+            v = N[random.randint(0, len(N) - 1)]
+        N.append(v)
+
+    return N
