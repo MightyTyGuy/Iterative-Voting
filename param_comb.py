a82143fa3f6806c9fcc47d83ced57adec77830d5
Unknown <tyler.bowles@aggiemail.usu.edu>
2018-04-29 16:49:11 -0600

    Removing data files and adding fixed Param_combo

--- param_comb.py
+++ param_comb.py
@@ -4,9 +4,9 @@ import generators
 
 meta_file_name = "meta.txt"
 res_file_name = "results.txt"
-meta_file = open(meta_file_name, 'w')
-res_file = open(res_file_name, 'w')
 
+with open(meta_file_name, 'w') as meta_file:
+    with open(res_file_name, 'w') as res_file:
         #add column labels to files (these will be set up like a csv)
         meta_file.write("ExpNum " + "UtilType " + "GenType " + "Alpha " + "Voters " \
                         + "Candidates " + "Iterations " + "Borda_borda " \
@@ -20,10 +20,7 @@ res_file = open(res_file_name, 'w')
                         + "Borda_LearningBestResponse " + "Cond_LearningBestResponse " \
                         + "Borda_Pragmatist " + "Cond_Pragmatist\n")
 
-meta_file.close()
-res_file.close()
-
-num_reps = 100
+num_reps = 1000
 num_iters = 400
 alpha = 0.1
 gens = [generators.impartial_culture, generators.polya_eggenberger]
@@ -43,8 +40,8 @@ for util in utils:
                 print("Current Params:", (util.__name__, gen.__name__, num_cands, num_voters))
 
                 # Create variables for experiment data
-                borda_avg = [{'learn': 0, 'bestres': 0, 'bayes': 0, 'learnbr': 0, 'prag': 0} for _ in range(num_reps)]
-                cond_avg = [{'learn': 0, 'bestres': 0, 'bayes': 0, 'learnbr': 0, 'prag': 0} for _ in range(num_reps)]
+                borda_avg = [{'learn': 0, 'bestres': 0, 'bayes': 0, 'learnbr': 0, 'prag': 0} for _ in range(num_iters)]
+                cond_avg = [{'learn': 0, 'bestres': 0, 'bayes': 0, 'learnbr': 0, 'prag': 0} for _ in range(num_iters)]
 
                 meta_borda_avg = {'borda': 0, 'plur': 0, 'cope': 0, 'stv': 0}
                 meta_cond_avg = {'borda': 0, 'plur': 0, 'cope': 0, 'stv': 0}
@@ -53,7 +50,7 @@ for util in utils:
 
                 # Run each repetition
                 for rep in range(num_reps):
-                    exp = experiment(util, gen, num_cands, num_voters, exp_num, rep, num_iters, alpha)
+                    exp = experiment(util, gen, num_cands, num_voters, num_iters, alpha)
                     isCond = exp._isCond
 
                     # Collect experiment data
@@ -64,7 +61,7 @@ for util in utils:
                     # Borda Ratio
                     for elec_type in static_borda:
                         meta_borda_avg[elec_type] = (1 / (rep + 1)) * (rep * meta_borda_avg[elec_type] + static_borda[elec_type])
-                    for i in len(iter_borda):
+                    for i in range(num_iters):
                         for agent_type in iter_borda[i]:
                             borda_avg[i][agent_type] = (1 / (rep + 1)) * (rep * borda_avg[i][agent_type] + iter_borda[i][agent_type])
 
@@ -72,13 +69,17 @@ for util in utils:
                     if isCond:
                         for elec_type in static_cond:
                             meta_cond_avg[elec_type] = (1 / (cond_counter + 1)) * (cond_counter * meta_cond_avg[elec_type] + static_cond[elec_type])
-                        for i in len(iter_cond):
+                        for i in range(num_iters):
                             for agent_type in iter_cond[i]:
                                 cond_avg[i][agent_type] = (1 / (cond_counter + 1)) * (cond_counter * cond_avg[i][agent_type] + iter_cond[i][agent_type])
                         cond_counter += 1
+
+                    # Progress
+                    if (rep + 1) % 100 == 0:
+                        print("Repetition", str(rep+1), "/", str(num_reps))
                 
                 # Write data to file
-                with open(meta_file_name, 'w') as meta:
+                with open(meta_file_name, 'a') as meta:
                     meta.write(str(exp_num) + " " +  util.__name__ + \
                        " " + gen.__name__ + " " + str(alpha) + " " + \
                        str(num_voters) + " " + str(num_cands) + " " + str(num_iters) + " " + \
@@ -87,7 +88,7 @@ for util in utils:
                        + " " + str(meta_cond_avg['cope']) + " " + str(meta_cond_avg['plur']) + " " + \
                        str(meta_cond_avg['stv']) + "\n")
 
-                with open(res_file_name, 'w') as res:
+                with open(res_file_name, 'a') as res:
                     for i in range(num_iters):
                         res.write(str(exp_num) + " " + str(i) + " " + \
                                 str(borda_avg[i]['learn']) + " " + str(cond_avg[i]['learn']) + " " + \
@@ -96,5 +97,5 @@ for util in utils:
                                 str(borda_avg[i]['learnbr']) + " " + str(cond_avg[i]['learnbr']) + " " + \
                                 str(borda_avg[i]['prag']) + " " + str(cond_avg[i]['prag']) + "\n")
                 
-                
+                # Progress
                 print("Completed", exp_num, "/", num_combs, "Experiments")
